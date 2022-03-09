02541a1e00197e155fa9d7a2fe11f5d235ec8f35
2azmi2 <azmikamikaze@gmail.com>
2022-03-09 12:54:52 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<html>
+	<head>
+		<title>Pengenalan Diri</title>
+		<style>
+			body{
+				background: url('img/bg1.png'), url('img/bg2.png');
+				background-repeat: no-repeat, no-repeat;
+				background-position: left, right;
+				background-attachment: fixed, fixed;
+				background-color: grey;
+			}
+			div.fix{
+				position: fixed;
+				top: 0;
+				right: 0;
+				width: 200px;
+				height: 100px;
+				color: white;
+				font-size: 30pt;
+			}
+			div.sticky{
+				top: 0;
+				color: lightblue;
+				font-family: comic sans MS;
+				text-align: center;
+				font-size: 30pt;
+			}
+			.myself{
+				text-align: center;
+				padding-left: 10px;
+				font-size: 14pt;
+				line-height: 25px;
+				color: black;
+				background-color: white;
+				margin-left: 440px;
+				height: 850px;
+				width: 450px;
+			}
+			.profile{
+				text-align: left;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="sticky">About Me</div>
+		<br>
+		<div class="fix">
+			ANIME LOVER
+		</div>
+		<div class="myself">
+			<br>
+			<img class="rom1" src="img/me.jpg" height="450" width="400" align="center"><br><br>
+			<div class="profile">
+				Nama : R.Muhammad Azmi Herdi Shofiyullah<br>
+				Hobi : Menonton Anime dan Membaca Manga<br>
+				Email : <a href="azmikamikaze@gmail.com">azmikamikaze@gmail.com</a><br>
+				Status : Kuliah - Mahasiswa di Polinema<br>
+				TTL : Jember, 12 September 2001<br>
+				Alasan menjadi Anime Lover : <br>
+				Karena gambar dan grafik yang sangat memukau serta memperlihatkan
+				betapa luasnya dunia. Selain itu juga karena menonton anime dan 
+				membaca manga mampu mengasah imajinasi kita sehingga kita bisa 
+				menjadi lebih kreatif dalam bertindak dan juga anime memberi
+				banyak pelajaran hidup kepada saya.
+			</div>
+		</div>
+	</body>
+</html>
